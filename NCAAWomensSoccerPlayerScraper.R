c0266b2c11429374ed16a18e9e43a90afce2c87d
Derek Willis <dwillis@gmail.com>
2024-09-05 11:38:43 -0400

    fixed scraper

--- NCAAWomensSoccerPlayerScraper.R
+++ NCAAWomensSoccerPlayerScraper.R
@@ -3,8 +3,8 @@ library(lubridate)
 library(rvest)
 library(janitor)
 
-urls <- read_csv("url_csvs/ncaa_womens_soccer_teamurls_2023.csv") %>% pull(2)
-season = "2023"
+urls <- read_csv("url_csvs/ncaa_womens_soccer_teamurls_2024.csv") %>% pull(2)
+season = "2024"
 
 root_url <- "https://stats.ncaa.org"
 playerstatstibble = tibble()
@@ -15,11 +15,12 @@ for (i in urls){
   
   schoolpage <- i %>% read_html()
   
-  schoolfull <- schoolpage %>% html_nodes(xpath = '//*[@id="contentarea"]/fieldset[1]/legend/a[1]') %>% html_text()
+  schoolfull <- schoolpage %>% html_nodes(xpath = '/html/body/div[2]/div/div/div/div/div/div[1]/a') %>% html_text()
+#  schoolfull <- schoolpage %>% html_nodes(xpath = '//*[@id="contentarea"]/fieldset[1]/legend/a[1]') %>% html_text()
   
   playerstats <- schoolpage %>% html_nodes(xpath = '//*[@id="stat_grid"]') %>% html_table()
   
-  playerstats <- playerstats[[1]] %>% filter(Player != "TEAM" & Player != "Totals" & Player != "Opponent Totals") %>% mutate(RosterName = Player) %>% separate(Player, into=c("LastName", "FirstName"), sep=",") %>% mutate(FullName = paste(FirstName, LastName, sep=" ")) %>% mutate(Team = schoolfull, Season=season) %>% clean_names() %>% select(season, team, jersey, full_name, roster_name, first_name, last_name, yr, pos, everything()) %>% mutate_at(vars(-season, -team, -jersey, -full_name, -roster_name, -first_name, -last_name, -yr, -pos, -goal_app), ~str_replace(., ",", "")) %>%  mutate_at(vars(-season, -team, -jersey, -full_name, -roster_name, -first_name, -last_name, -yr, -pos, -goal_app, -ggs, -goalie_min_plyd), as.numeric)
+  playerstats <- playerstats[[1]] %>% filter(Player != "TEAM" & Player != "Totals" & Player != "Opponent Totals") %>% mutate(RosterName = Player) %>% separate(Player, into=c("LastName", "FirstName"), sep=",") %>% mutate(FullName = paste(FirstName, LastName, sep=" ")) %>% mutate(Team = schoolfull, Season=season) %>% clean_names() %>% select(season, team, full_name, roster_name, first_name, last_name, yr, pos, everything()) %>% mutate_at(vars(-season, -team, -full_name, -roster_name, -first_name, -last_name, -yr, -pos), ~str_replace(., ",", "")) %>%  mutate_at(vars(-season, -team, -full_name, -roster_name, -first_name, -last_name, -yr, -pos), as.numeric)
   
   message <- paste0("Fetching ", schoolfull)
   
