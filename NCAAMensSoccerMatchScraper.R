2f46a2c3f70958b41431c6e30675d99339457ecd
Derek Willis <dwillis@gmail.com>
2025-11-09 18:52:58 -0500

    fixed rename_with()

--- NCAAMensSoccerMatchScraper.R
+++ NCAAMensSoccerMatchScraper.R
@@ -14,13 +14,13 @@ matchstatstibble = tibble()
 matchstatsfilename <- paste0("data/ncaa_mens_soccer_matchstats_", season, ".csv")
 
 for (i in urls){
-  
+
   team_id = str_split(str_split(i, '&', n=3)[[1]][[1]], '=')[[1]][[2]]
-  
+
   schoolpage <- i %>% read_html()
-  
+
   schoolfull <- schoolpage %>% html_nodes(xpath = '/html/body/div[2]/div/div/div/div/div/div[1]/a') %>% html_text()
-  
+
   if(length(schoolfull) == 0) {
     card_text <- schoolpage %>% html_nodes(".card-header") %>% html_text() %>% .[1]
     if(!is.na(card_text)) {
@@ -66,7 +66,7 @@ for (i in urls){
   }
 
   matches <- schoolpage %>% html_nodes(xpath = '//*[@id="game_breakdown_div"]/table') %>% html_table(fill=TRUE)
-  
+
   matches <- matches[[1]] %>% slice(3:n()) %>%
     row_to_names(row_number = 1) %>%
     clean_names() %>%
@@ -84,10 +84,10 @@ for (i in urls){
     clean_names() %>%
     mutate_at(vars(-date, -opponent, -home_away, -outcome, -team), ~str_replace(., "/", "")) %>%
     mutate_at(vars(-date, -team, -opponent, -home_away, -outcome), as.numeric)
-  
+
   teamside <- matches %>% filter(opponent != "Defensive Totals")
 
-  opponentside <- matches %>% filter(opponent == "Defensive Totals") %>% select(-opponent, -home_away, -overtime, -games) %>% rename_with(.cols = 6:18, function(x){paste0("defensive_", x)})
+  opponentside <- matches %>% filter(opponent == "Defensive Totals") %>% select(-opponent, -home_away, -overtime, -games) %>% rename_with(.cols = 6:16, function(x){paste0("defensive_", x)})
 
   joinedmatches <- inner_join(teamside, opponentside, by = c("date", "team", "outcome", "team_score", "opponent_score"))
 
@@ -111,20 +111,20 @@ for (i in urls){
         goalie_g_ties = NA
       )
   }
-  
+
   # grab opponent IDs - the one issue here is that if a team plays an opponent that isn't linked, this won't work and the team's matches will not have any opponent_id values
   #opponent_ids <- schoolpage %>% html_nodes("a") %>% html_attr("href") %>% as_tibble() %>% filter(str_detect(value, "/teams/")) %>% filter(!str_detect(value, paste0("/", team_id, "/"))) %>% separate(value, into=c('blank', 'team', 'opponent_id', 'season_id'), sep = '/') %>% select(opponent_id)
-  
+
   #tryCatch(joinedmatches <- bind_cols(joinedmatches, opponent_ids),
   #         error = function(e){NA})
-  
+
   tryCatch(matchstatstibble <- bind_rows(matchstatstibble, joinedmatches),
            error = function(e){NA})
-  
+
   message <- paste0("Adding ", schoolfull)
-  
+
   print(message)
-  
+
   Sys.sleep(1)
 }
 
